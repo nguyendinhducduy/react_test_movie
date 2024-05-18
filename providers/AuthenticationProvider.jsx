f101e95eeb87a93202fc673c05ac5f70f32100bf
Dinh Duy <nguyenducdinhduy@gmail.com>
2024-05-18 21:56:06 +0700

    update

--- providers/AuthenticationProvider.jsx
+++ providers/AuthenticationProvider.jsx
@@ -17,13 +17,24 @@ export const useAuthentication = () => {
 export const AuthenticationProvider = ({ children }) => {
     const [username, setUsername] = useState('');
     const [isAuthenticate, setIsAuthenticate] = useState(false);
+
+     useEffect(() => {
+        setIsAuthenticate(localStorage.getItem('isLogin') === 'true');
+        setUsername(localStorage.getItem('isUser') || '');
+    }, []);
+ 
+
     const saveUsername = (name) => {
         setUsername(name.username);
         setIsAuthenticate(true);
+        localStorage.setItem("isLogin",true) ;
+        localStorage.setItem("isUser",name.username) ;
     }
     const logoutContext = () => {
         setUsername("");
         setIsAuthenticate(false);
+        localStorage.setItem("isLogin",false) ;
+        localStorage.setItem("isUser",null) ;
     }
     return <AuthenticationContext.Provider value={{ username, saveUsername, isAuthenticate , logoutContext }}>{children}</AuthenticationContext.Provider>
 }
